606e8a22cc7cafbdde254efa5108edd72c17a258
Adilet Zamirbekov <adiletto@adiletto-2.local>
2023-09-29 12:07:06 -0500

    made modal for logout

--- src/components/ProfileMenu/ProfileMenu.jsx
+++ src/components/ProfileMenu/ProfileMenu.jsx
@@ -1,17 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ProfileMenu.module.scss";
 import profIcon from "../../assets/profilePhoto.png";
 import heartIcon from "../../assets/heartIcon.png";
 import myProductsIcon from "../../assets/myProductsIcon.png";
 import logoutIcon from "../../assets/logOutIcon.png";
 import { RightOutlined } from "@ant-design/icons";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { Modal } from "antd";
+import trashIcon from "../../assets/logout.svg";
+import NextBtn from "../NextBtn/NextBtn";
 
 
 const ProfileMenu = () => {
+  const [isModalVisible, setIsModalVisible] = useState(false)
+  const navigate = useNavigate();
+
+  const handleToggleModal = () => {
+    setIsModalVisible(!isModalVisible);
+  }
+
+  const handleLogout = () => {
+    navigate("/");
+  }
+
   return (
     <div className={styles.profileMenu}>
-      <div className={styles.profileMenu__profileLink}>
+      <Link to="/profile" className={styles.profileMenu__profileLink}>
         <img
           className={styles.profileMenu__profileLink__icon}
           src={profIcon}
@@ -25,7 +39,7 @@ const ProfileMenu = () => {
             adiletto.kg@gmail.com
           </p>
         </div>
-      </div>
+      </Link>
       <Link to="/liked" className={styles.profileMenu__likedLink}>
         <img src={heartIcon} alt="heart-icon" />
         <h5 className={styles.profileMenu__header}>Favorites</h5>
@@ -36,11 +50,23 @@ const ProfileMenu = () => {
         <h5 className={styles.profileMenu__header}>My products</h5>
         <RightOutlined className={styles.profileMenu__rightIcon} />
       </Link>
-      <Link to="/" className={styles.profileMenu__logoutLink}>
+      <div className={styles.profileMenu__logoutLink} onClick={handleToggleModal}>
         <img src={logoutIcon} alt="logout-icon" />
         <h5 className={styles.profileMenu__header}>Log out</h5>
         <RightOutlined className={styles.profileMenu__rightIcon} />
-      </Link>
+      </div>
+
+      <Modal
+      className={styles.profileMenu__modal}
+      visible={isModalVisible}
+      onCancel={handleToggleModal}
+      footer={null}
+      >
+        <img src={trashIcon} alt="trash" className={styles.profileMenu__modal_icon} />
+        <h5 className={styles.profileMenu__modal_text}>Do you really want to exit the application?</h5>
+        <NextBtn text="Log out" onClick={handleLogout}/>
+        <NextBtn text="No"/>
+      </Modal>
     </div>
   );
 };
--- src/pages/MyProducts/MyProducts.jsx
+++ src/pages/MyProducts/MyProducts.jsx
@@ -1,9 +1,24 @@
-import React from 'react'
+import React from "react";
+import styles from "./MyProducts.module.scss";
+import ProfileMenu from "../../components/ProfileMenu/ProfileMenu";
+import Header from "../../components/Header/Header";
+import Card from "../../components/Card/Card";
 
 const MyProducts = () => {
+  const numOfCards = 12;
   return (
-    <div>MyProducts</div>
-  )
-}
+    <div className={styles.myProducts}>
+      <ProfileMenu />
+      <div className={styles.myProducts__rightBlock}>
+        <Header header="My products" />
+        <div className={styles.myProducts__rightBlock__container}>
+          {Array.from({ length: numOfCards }).map((_, index) => (
+            <Card key={index} />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default MyProducts
+export default MyProducts;
